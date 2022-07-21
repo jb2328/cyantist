d201e3927adf11f8524ff28d398b8fc8b892a58c
Justas Brazauskas <jb2328@cam.ac.uk>
2022-07-21 19:17:38 +0300

    init commit
    addded main Flask and Bootsrap main files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    #return "Hello, Cyantist!"
+
+@app.route("/test/")
+@app.route("/test/<name>")
+def test(name = None):
+    return render_template(
+        "test.html",
+        name=name,
+        date=datetime.now()
+    )
